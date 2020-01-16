1ae260a358ac0665067b03dac8158ac9bd6dfbd5
1Gregory <6squaress@gmail.com>
2020-01-16 22:56:20 +0300

    что-то

--- expeval.py
+++ expeval.py
@@ -1,6 +1,15 @@
+import math
+
+names = {
+    "pi": {
+        "val": math.pi
+    }
+}
+
+
 class ExpEval:
-    def __init__(self):
-        pass
+    def __init__(self, std_names):
+        self.std_names = std_names
 
     def comp_exp(self, query):
         pass
