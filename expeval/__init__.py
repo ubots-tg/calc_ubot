0310df4011c5a8d2cdb08af310caf9c27bb2bff5
1Gregory <6squaress@gmail.com>
2020-01-31 22:08:37 +0300

    Now taking ExpEval from side module on PyPi

--- expeval/__init__.py
+++
@@ -1,2 +0,0 @@
-from .expeval_std import std_names, std_specific_operators
-from .expeval import ExpEval, ExpEvalProcedure
--- expeval/ulib/__init__.py
+++
@@ -1,2 +0,0 @@
-from .useful import is_from_same, UsefulObj, Signal
-from .killable_thread import KillableThread
